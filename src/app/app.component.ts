da7cb3bb3dc9651819ab58678906a74c412b6bb2
Jorge Pires <jorgeguerrapires@yahoo.com.br>
2022-12-22 07:40:13 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+//TensorFlow.js
+import * as tf from '@tensorflow/tfjs';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+
+  ngOnInit() {
+    this.game();
+  }
+
+  game() {
+    tf.ready().then(() => {
+      const modelPath = './assets/model/ttt_model.json'
+
+      tf.tidy(() => {
+        tf.loadLayersModel(modelPath).then((model) => {
+          // Three board states
+          const emptyBoard = tf.zeros([9])
+          const betterBlockMe = tf.tensor([-1, 0, 0, 1, 1, -1, 0, 0, -1])
+
+          const betterBlockMe2 = tf.tensor([-1, 0, 1, 0, -1, 0, 0, 0, 0])
+
+          const goForTheKill = tf.tensor([1, 0, 1, 0, -1, -1, -1, 0, 1])
+
+
+          // Stack states into a shape [3, 9]
+          const matches = tf.stack([emptyBoard, betterBlockMe2, goForTheKill])
+          const result = model.predict(matches) as tf.Tensor
+
+          // Log the results
+          result.reshape([3, 3, 3]).print()
+
+        })
+      })
+    })
+  }
 }
